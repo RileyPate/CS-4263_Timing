88bce604405c737f6b0c07f5fd19e848f2f2032e
Riley Pate <riley1234@sbcglobal.net>
2019-09-25 17:17:36 -0500

    Updates

--- TimingRandom2/src/Threading.java
+++ TimingRandom2/src/Threading.java
@@ -7,17 +7,18 @@ import org.jsoup.select.Elements;
 class TimingThread extends Thread 
 { 
 
-	public void start(String string) {
+	public void start(String info,String string) {
 		try {
     		long beginning = System.nanoTime();
 			Document document = Jsoup.connect(string).get();
 			Elements elements = document.select("body");
-			/*for(Element i: elements) {
-				System.out.println(i.text());
-			}*/
+			
 			long ending = System.nanoTime();
 			double total =(double)(ending - beginning)/1000000000;
-			System.out.println(string +" took "+total+ " seconds to run.");
+			for(Element i: elements) {
+				System.out.println(info +string +" "+total+ " "+ i.text());
+			}
+			
 		}
 		catch (Exception e){
 			System.out.println(string+ " Failed to connect");
@@ -28,17 +29,30 @@ class TimingThread extends Thread
 // Main Class 
 public class Threading 
 { 
+	/* OLD IP ADDRESS FOR US REGION
 	public static String[] sites = { 
 	"http://35.192.236.37:8080/number.jsp",
 	"http://35.222.240.90/",
 	"http://rng-python.appspot.com"};
+	*/
+	public static String[] sites = { 
+			"http://34.97.25.195",
+			"http://34.97.51.216:8080/number.jsp",
+			"http://rng-python-asiane2.appspot.com"};
+	public static String[] info = {
+			"asia-northeast_2a_VM_Python@",
+			"asia-northeast2_a_VM_Java@",
+			"asia-northeast2_a_app_Python@"
+	};
+	
+	
     public static void main(String[] args) 
     { 
         int n = 8; // Number of threads 
         for (int i=0; i<sites.length; i++) 
         { 
             TimingThread object = new TimingThread(); 
-            object.start(sites[i]); 
+            object.start(info[i].sites[i]); 
         } 
     } 
 }
